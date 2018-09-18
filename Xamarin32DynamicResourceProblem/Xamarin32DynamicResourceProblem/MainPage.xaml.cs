8ef3baf5782c0dbb57e746b754e2ec45295043ca
Radoslaw Kubas <rk@vrspot.es>
2018-09-19 00:08:30 +0200

    Add project files.

---
+++ Xamarin32DynamicResourceProblem/Xamarin32DynamicResourceProblem/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Xamarin32DynamicResourceProblem
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyLongStyle(object sender, EventArgs e)
+        {
+            Resources["paddingStyle"] = Resources["longStyle"];
+
+        }
+
+        private void ApplyShortStyle(object sender, EventArgs e)
+        {
+            Resources["paddingStyle"] = Resources["shortStyle"];
+        }
+    }
+}
